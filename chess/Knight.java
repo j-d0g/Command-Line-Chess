67c4bd77fd7529af4ec5ddfda7df6f8148b434ca
Jordan Tran <jordan.tran@student.manchester.ac.ukgit config --global user.email jordan.tran@student.manchester.ac.uk>
2021-03-28 15:04:27 +0100

    Knight implemented

--- chess/Knight.java
+++ chess/Knight.java
@@ -1,7 +1,12 @@
 package chess;
 
-public class Knight extends Piece
-{
+public class Knight extends Piece {
+
+  //displayInvalidMove --Helper
+	private boolean displayInvalidMove(){
+		System.out.println("You can't move there.");
+		return false;
+	}
 
   public Knight(PieceColour p){
     if (p == PieceColour.WHITE){
@@ -16,7 +21,24 @@ public class Knight extends Piece
 
   @Override
   public boolean isLegitMove(int i0, int j0, int i1, int j1){
-    return true;
+
+    int iDiff = i1 - i0;
+    int jDiff = j1 - j0;
+    int iSize = Math.abs(iDiff);
+    int jSize = Math.abs(jDiff);
+    int ijTotal = iSize + jSize;
+    Piece p = Board.getPiece(i0, j0);
+    Piece q = Board.getPiece(i1, j1);
+
+    if ((iSize == 2 || jSize == 2) && ijTotal == 3){
+      if (Board.hasPiece(i1, j1) && p.getColour() == q.getColour()){
+        System.out.println("You can't capture your own piece!");
+        return false;
+      }
+      else{
+        return true;
+      }
+    }
+    return displayInvalidMove();
   }
-  
 }
